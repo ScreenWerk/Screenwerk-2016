0967cd52d34bde09bfe8c37c1bc4ae61d91b4f26
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-11-25 11:29:59 +0200

    make sure iframe has contentWindow

--- app/js/sync.js
+++ app/js/sync.js
@@ -23,7 +23,7 @@ module.exports.fetchConfiguration = (_G, callback) => {
     _G.playbackLog.log('Requesting ' + conf_url)
     console.log(_G.packageJson.productName + ' ' + _G.packageJson.version + '@' + _G.gitBranch)
     let options = {
-      headers: { 'User-Agent': _G.packageJson.productName + ' ' + _G.packageJson.version + '@' + _G.gitBranch },
+      headers: { 'User-Agent': _G.packageJson.productName + ' ' + _G.packageJson.version + '@' + _G.gitBranch + ';E@' + _G.packageJson.devDependencies.electron},
       uri: conf_url
     }
     request(options, function(error, response, data) {
